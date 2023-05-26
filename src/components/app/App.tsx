15154b960c1a03612d646bac0ebbc50506c847c8
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-05-26 15:24:54 -0700

    Adds file setup

---
+++ src/components/app/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Error from '../error/Error'
+import Form from '../form/Form'
+import Header from '../header/Header'
+import Images from '../images/Image'
+import Meme from '../meme/Meme'
+import SavedMemes from '../savedmemes/SavedMemes'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header name='fellow human'/>
+    </div>
+  );
+}
+
+export default App;
