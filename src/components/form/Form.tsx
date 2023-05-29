a7b999afbc8f0fbd110ebc66ab8d4815fa0c9cdd
Sharie Trachsel <strachsel22@turing.edu>
2023-05-29 08:08:53 -0500

    Add styling to form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -10,17 +10,17 @@ interface Joke {
 	joke: string;
 }
 
-interface SavedMeme extends Joke{
+interface SavedMeme extends Joke {
 	image: string;
 	id: string;
 }
 
 interface FormState {
-  jokes: Joke[];
-  error: string;
-  selectedImage: string;
-  selectedJoke: string;
-  savedMeme: SavedMeme; 
+	jokes: Joke[];
+	error: string;
+	selectedImage: string;
+	selectedJoke: string;
+	savedMeme: SavedMeme;
 }
 
 interface FormProps {
@@ -29,38 +29,38 @@ interface FormProps {
 
 // component 
 class Form extends React.Component<FormProps, FormState> {
-	constructor(props: FormProps)	{
+	constructor(props: FormProps) {
 		super(props)
 		this.state = {
 			jokes: [],
 			error: "",
 			selectedImage: this.props.selectedImage,
 			selectedJoke: "",
-			savedMeme: { image: "", joke: "", id: ""}
+			savedMeme: { image: "", joke: "", id: "" }
 		}
 	}
 
-// lifecycle methods 
-	componentDidMount = ()	=>	{
+	// lifecycle methods 
+	componentDidMount = () => {
 		this.getJokeOptions()
 	}
 
-// methods
+	// methods
 	selectJoke = (joke: string) => {
 		this.setState({ selectedJoke: joke }, () => {
 		});
 	}
 
-	getJokeOptions = () =>{
+	getJokeOptions = () => {
 		getJokes()
-		.then((data) => {
-			const jokes: Joke[] = data.map((joke: Joke) => joke);
-			this.setState({ jokes: jokes });
-		})
-		.catch((error) => this.setState({ error: error.message }));
+			.then((data) => {
+				const jokes: Joke[] = data.map((joke: Joke) => joke);
+				this.setState({ jokes: jokes });
+			})
+			.catch((error) => this.setState({ error: error.message }));
 	}
 
-	saveMeme = ()	=>	{
+	saveMeme = () => {
 		const newMeme: SavedMeme = {
 			image: this.state.selectedImage,
 			joke: this.state.selectedJoke,
@@ -70,18 +70,18 @@ class Form extends React.Component<FormProps, FormState> {
 		this.clearInputs();
 	}
 
-	clearInputs	=	()	=>	{
+	clearInputs = () => {
 		this.setState({ selectedImage: "" });
 		this.setState({ selectedJoke: "" });
 	}
 
-// component render
-	render = ()	=>	{
+	// component render
+	render = () => {
 		const { jokes, selectedImage, selectedJoke } = this.state;
 
-    const options: JSX.Element[] = jokes.map((joke, index) => {
-      return (
-				<div key={index + 1}className="joke-option-wrapper">
+		const options: JSX.Element[] = jokes.map((joke, index) => {
+			return (
+				<div key={index + 1} className="joke-option-wrapper">
 					<input
 						type="radio"
 						id={`joke${index + 1}`}
@@ -93,30 +93,33 @@ class Form extends React.Component<FormProps, FormState> {
 					<label htmlFor={`joke${index + 1}`} id={`joke${index + 1}`} className="joke-option"> {joke.joke} </label>
 				</div>
 			)
-    })
+		})
 
-    if (jokes.length < 0) {
-      return <div>Loading...</div>
-    }
+		if (jokes.length < 0) {
+			return <div>Loading...</div>
+		}
 
-    return (
+		return (
+
 			<div className="generator-container">
-				<Meme 
-					selectedJoke={selectedJoke} 
+				<Meme
+					selectedJoke={selectedJoke}
 					selectedImage={selectedImage}
 				/>
-				<form className="form-container">
-					<button>X</button>
-					<h4 className="joke-option-header">Choose Your Joke</h4>
-					{options}
-      	</form>
+				<div className="form-wrapper">
+						<button className="close-button">X</button>
+					<form className="form-container">
+						<h4 className="joke-option-header">Choose Your Joke</h4>
+						{options}
+					</form>
 					<div className="button-wrapper">
 						<button className="button" onClick={this.getJokeOptions}>get new jokes</button>
 						<button className="button" onClick={this.saveMeme}>save meme</button>
 					</div>
+				</div>
 			</div>
-    );
-  }
+		);
+	}
 }
 
 export default Form
