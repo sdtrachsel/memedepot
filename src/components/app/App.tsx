acd6d1b75a5a53851ff32ff5389e901c9dc3bcbf
Sharie Trachsel <strachsel22@turing.edu>
2023-06-02 13:24:58 -0500

    Responseive styling to main

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -20,14 +20,14 @@ class App extends React.Component<{}, AppState>{
     }
   }
 
-  saveNewMeme = (newMeme:SavedMeme) => {
-    this.setState({savedMemes:[...this.state.savedMemes, newMeme]})
+  saveNewMeme = (newMeme: SavedMeme) => {
+    this.setState({ savedMemes: [...this.state.savedMemes, newMeme] })
   }
 
   favoriteMeme = (memeId: string) => {
-    const {savedMemes} = this.state;
-    const updatedMemes = savedMemes.map((meme)=> {
-      if(meme.id === memeId){
+    const { savedMemes } = this.state;
+    const updatedMemes = savedMemes.map((meme) => {
+      if (meme.id === memeId) {
         return {
           ...meme,
           favorite: !meme.favorite
@@ -36,34 +36,34 @@ class App extends React.Component<{}, AppState>{
       return meme;
     })
 
-    this.setState({savedMemes: updatedMemes})
+    this.setState({ savedMemes: updatedMemes })
   }
 
   render(): React.ReactNode {
     return (
       <div className="App">
 
-        <Header savedMemes={this.state.savedMemes}/>
+        <Header savedMemes={this.state.savedMemes} />
 
-        <Route exact path="/" render={() => <Images saveNewMeme={this.saveNewMeme} />} /> 
+        <Route exact path="/" render={() => <Images saveNewMeme={this.saveNewMeme} />} />
 
-        <Route exact path="/:id" render={( { match } ) => {
+        <Route exact path="/:id" render={({ match }) => {
           const memeId = match.params.id
           const findMeme = this.state.savedMemes.find(meme => meme.id === memeId)
 
-          if(!findMeme) {
-            //Error page or redirect to home
+          if (!findMeme) {
+            <Error />
           } else {
-            return(
+            return (
               <div className='single-view-wrapper'>
-              <Meme selectedJoke={findMeme.joke} selectedImage={findMeme.image}/>
+                <Meme selectedJoke={findMeme.joke} selectedImage={findMeme.image} />
               </div>
             )
           }
         }} />
 
-        <Route exact path="/savedmemes" render={() => <SavedMemes favoriteMeme={this.favoriteMeme} savedMemes={this.state.savedMemes}/>} />
-        
+        <Route exact path="/savedmemes" render={() => <SavedMemes favoriteMeme={this.favoriteMeme} savedMemes={this.state.savedMemes} />} />
+
       </div>
     );
   }
