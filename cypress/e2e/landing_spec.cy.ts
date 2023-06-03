c0e4fdb5c80bb4bbf437395cb295ad7a35b3a0d4
Sharie Trachsel <strachsel22@turing.edu>
2023-06-03 15:57:52 -0500

    Update Cypress landing page image count

--- cypress/e2e/landing_spec.cy.ts
+++ cypress/e2e/landing_spec.cy.ts
@@ -1,45 +1,44 @@
 {
-
-describe('home page', () => {
-  beforeEach(() => {
-    cy.intercept('GET', 'https://api.api-ninjas.com/v1/dadjokes?limit=3', {
-      statusCode: 200,
-      fixture: 'jokes1.json'
+  describe('home page', () => {
+    beforeEach(() => {
+      cy.intercept('GET', 'https://api.api-ninjas.com/v1/dadjokes?limit=3', {
+        statusCode: 200,
+        fixture: 'jokes1.json'
+      });
+      cy.visit('http://localhost:3000/');
     });
-    cy.visit('http://localhost:3000/');
-  });
-  
-  it('it should display logo', () => {
-    cy.get('.logo').should('be.visible').and('have.attr', 'src').and('include', 'meme_depot_logo.5a433c14dea18bd747d9.png')
-  });
 
-  it('should dislay Home and Saved Meme nav links', () => {
-    cy.get('.header-button')
-    .should('contain', 'Home')
-  });
+    it('it should display logo', () => {
+      cy.get('.logo').should('be.visible').and('have.attr', 'src').and('include', 'meme_depot_logo.5a433c14dea18bd747d9.png')
+    });
 
-  it('should have 19 images upon landing', () => {
-    cy.get('.button-image').should('have.length', 19)
-  });
+    it('should dislay Home and Saved Meme nav links', () => {
+      cy.get('.header-button')
+        .should('contain', 'Home')
+    });
 
-  it('first image should be a picture of Ron Swanson', () => {
-    cy.get('.button-image').first().and('have.attr', 'src').and('include', 'https://i.imgur.com/6AgsAtM.png')
-  });
+    it('should have 18 images upon landing', () => {
+      cy.get('.button-image').should('have.length', 18)
+    });
 
-  it('the last image should be a picture of Bob Belcher', () => {
-    cy.get('.button-image').last().and('have.attr', 'src').and('include', 'https://i.imgur.com/JjzYBO5.png')
-  });
+    it('first image should be a picture of Ron Swanson', () => {
+      cy.get('.button-image').first().and('have.attr', 'src').and('include', 'https://i.imgur.com/6AgsAtM.png')
+    });
 
-  it('should open meme generator', () => {
-    cy.get('.button-image').first().click().get('.generator-container')
-  });
+    it('the last image should be a picture of Bob Belcher', () => {
+      cy.get('.button-image').last().and('have.attr', 'src').and('include', 'https://i.imgur.com/JjzYBO5.png')
+    });
 
-  it('should notify users of bad URL paths', () => {
-    cy.visit('http://localhost:3000/test')
-    .get('.error-message').first()
-    .should('contain', "Something went wrong.")
-    .get('.error-message').last()
-    .should('contain', "URL not found.")
-  });
-})
+    it('should open meme generator', () => {
+      cy.get('.button-image').first().click().get('.generator-container')
+    });
+
+    it('should notify users of bad URL paths', () => {
+      cy.visit('http://localhost:3000/test')
+        .get('.error-message').first()
+        .should('contain', "Something went wrong.")
+        .get('.error-message').last()
+        .should('contain', "URL not found.")
+    });
+  })
 }
